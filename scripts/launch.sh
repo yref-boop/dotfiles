753f336173caf2e009bc7920ffc541cc51f9edbf
yref-boop <regofdez.yago@gmail.com>
2022-02-09 12:01:32 +0100

    added zero bar and reorganized left side

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,8 +1,9 @@
 killall -q polybar
+polybar -r zero &
 polybar -r first &
 polybar -r second &
 polybar -r third &
 polybar -r fourth &
 polybar -r fifth &
 polybar -r sixth &
-polybar -r seven &
+polybar -r seventh &
