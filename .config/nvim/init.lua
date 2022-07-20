6a8417be55d7b74c738c5008012e778f56e9f46f
yref-boop <regofdez.yago@gmail.com>
2022-07-20 15:59:07 +0200

    feat: nvim config for several plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,7 @@
 require "yref-boop.options"
+require "yref-boop.plugins"
+require "yref-boop.lualine"
+require "yref-boop.lspconfig"
+require "yref-boop.gitsigns"
+
+vim.cmd "colorscheme catppuccin"
