b91438a830bf7082ab84f6d70befcfa8d9c50839
yref-boop <regofdez.yago@gmail.com>
2022-09-11 13:26:04 +0200

    changed picom blur, shadows & animations

---
+++ .config/nvim/init.lua
@@ -0,0 +1,7 @@
+require "yref-boop.options"
+require "yref-boop.plugins"
+require "yref-boop.gitsigns"
+require "yref-boop.lualine"
+require "yref-boop.lspconfig"
+
+vim.cmd "colorscheme meliora"
---
+++ .config/nvim/lua/yref-boop/options.lua
@@ -0,0 +1,16 @@
+o = vim.opt
+
+o.mouse = "a"				-- allow mouse interaction in nvim
+o.tabstop = 4				-- set the number of spaces in a tab to 4
+o.smartindent = true			-- autoindent
+o.shiftwidth = 4			-- number of spaces by indentation
+o.breakindent = true			-- wrap lines mantain indentation
+o.expandtab = true			-- tabs to spaces
+
+o.clipboard = "unnamedplus"		-- allows neovim to access system clipboard
+o.ignorecase = true 			-- ignore case in search patterns
+
+vim.wo.fillchars='eob: '		-- change the ~ character to a space on non-written lines
+o.showmode = false			-- do not write the mode on the bottom
+o.guifont = "monospace:h15"		-- font used in graphical applications
+o.termguicolors = true 			-- set terminal gui colors
