518529968b545bf6a7e2b67f72be042e9709e259
yref-boop <regofdez.yago@gmail.com>
2023-02-05 20:25:58 +0100

    feat: changed some nvim options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,26 +1,35 @@
 o = vim.opt
 
-o.mouse = "a"                   -- allow mouse interaction in nvim
+
+-- basics
+
 o.tabstop = 4                   -- set the number of spaces in a tab to 4
 o.smartindent = true            -- autoindent
 o.shiftwidth = 4                -- number of spaces by indentation
 o.breakindent = true            -- wrap lines mantain indentation
 o.expandtab = true              -- tabs to spaces
+o.autoindent = true             -- add tabs automatically
+vim.wo.fillchars='eob: '        -- change non-written lines character
 
 
-o.clipboard = "unnamed,unnamedplus"     -- allows neovim to access system clipboard
+-- clipboard & search
+
+o.clipboard = "unnamed,unnamedplus"     -- access system clipboard
+
 o.ignorecase = true             -- ignore case in search patterns
+o.smartcase = true              -- if UPPERCASE on search, supposes case-sensitive
+o.iskeyword:append("-")         -- "-" now considered part of a word
 
-vim.wo.fillchars='eob: '        -- change the ~ character to a space on non-written lines
-o.showmode = false              -- do not write the mode on the bottom
+-- appereance
 
 o.guifont = {                   -- font used in graphical applications
     "Source Code Pro", ":h7.5:b"
 }
-
+o.showmode = false              -- do not write the mode on the bottom
 o.termguicolors = true          -- set terminal gui colors
 o.number = true                 -- show numberline
 o.relativenumber = true         -- show relative numbers
+
 
 -- neovide options
 
