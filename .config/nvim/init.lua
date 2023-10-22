e2886a31d226ec37d48f2f0f8af5e9026ce13a27
yref-boop <regofdez.yago@gmail.com>
2023-10-22 17:05:37 +0200

    feat: lazy packet manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,32 +1,15 @@
--- general requires
-require "options"
-require "plugins"
-require "lsp_config"
-require "git_signs"
-require "colors"
-require "bar"
+-- bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
--- force treesitter to use git, not curl
-require("nvim-treesitter.install").prefer_git = true
-require "treesitter"
-
--- limit severity of lsp messages
-vim.diagnostic.config({
-    virtual_text = {severity = {min = vim.diagnostic.severity.WARN}},
-    signs = {severity = {min = vim.diagnostic.severity.WARN}},
-    underline = {severity = {min = vim.diagnostic.severity.WARN}},
-})
-
--- blocks & spaces indicator
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-require("indent_blankline").setup {
-    show_end_of_line = true,
-    space_char_blankline = " ",
-}
-
--- vertical column
-vim.g.virtcolumn_char = '▕'     -- char to display the line
-vim.g.virtcolumn_priority = 10  -- priority of extmark
-vim.cmd("set colorcolumn=70")
-vim.cmd("hi Normal guibg = #100910")
+require("lazy").setup(plugins, opts)
