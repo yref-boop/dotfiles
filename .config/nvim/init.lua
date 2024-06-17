3c7109e5fba963b7f13cd792acb6da014e7933cb
yref-boop <yref.boop@gmail.com>
2024-06-17 20:37:09 +0200

    feat: added lazy package manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,36 @@
 require "config.keymaps"
 require "config.options"
+
+-- lazy bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- lazy options
+local options = {
+  defaults = { lazy = true },
+  install = { colorscheme = { catppuccin }},
+  change_detection = { notify = true },
+}
+
+-- set <space as leader>
+vim.g.mapleader = " "
+
+require("lazy").setup(
+  {
+    -- load lua/plugins & lsp
+    {import = 'plugins'},
+    {import = 'plugins.lsp'}
+  },
+  options
+  )
