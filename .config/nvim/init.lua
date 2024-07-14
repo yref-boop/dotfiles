619cda5df7ccb3647a14c575a4d0464f8a7f9f33
yref-boop <yref.boop@gmail.com>
2024-07-14 18:28:11 +0200

    feat: added lazy bootstrap

---
+++ .config/nvim/init.lua
@@ -0,0 +1,28 @@
+-- bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- lazy options
+local options = {
+  defaults = { lazy = true },
+  install = { colorscheme = { catppuccin }},
+  change_detection = { notify = true },
+}
+
+-- set <space as leader>
+vim.g.mapleader = " "
+
+require("lazy").setup(
+  { },
+  options
+)
