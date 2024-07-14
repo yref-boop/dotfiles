dc1d134a0c36f50e82d192268f418255d45a2f83
yref-boop <yref.boop@gmail.com>
2024-07-14 18:42:00 +0200

    feat: added basic nvim options

---
+++ .config/nvim/config/options.lua
@@ -0,0 +1,48 @@
+local vim = vim
+local opt = vim.opt
+
+
+-- tabs & indent
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.smartindent = true
+opt.breakindent = true
+opt.expandtab = true
+opt.autoindent = true
+
+
+-- search & clip
+opt.incsearch = false
+opt.smartcase = true
+opt.iskeyword:append("-")
+opt.clipboard:append("unnamedplus")
+
+
+-- appearance
+opt.number = true
+opt.showmode = true
+opt.relativenumber = true
+opt.termguicolors  = true
+opt.cmdheight = 1
+opt.completeopt = "menuone,noinsert,noselect"
+opt.list = true
+opt.colorcolumn = '80'
+vim.cmd('hi Normal guibg=#00000000')
+
+
+-- blank indicators
+opt.listchars:append "space:⋅"
+vim.wo.fillchars ='eob: '
+opt.guicursor = ""
+
+
+-- behaviour
+opt.scrolloff = 10
+opt.hidden = true
+opt.errorbells = false
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+opt.undodir = vim.fn.expand("~/.nvim/undodir")
+opt.backspace = "indent,eol,start"
