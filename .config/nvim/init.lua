ef6f2c6ec1cdec901c8d8db4463dc238c02de828
yref.boop <yref.boop@gmail.com>
2024-09-23 13:51:53 +0200

    refactor: separated config into options and lazy-packages modules

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,60 +1,32 @@
-local vim = vim
-local opt = vim.opt
+require "options"
 
--- controls
+-- bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- lazy configuration
+local options = {
+  defaults = { lazy = true },
+  install = { colorscheme = { catppuccin }},
+  change_detection = { notify = true },
+}
+
+-- set <space as leader>
 vim.g.mapleader = " "
-opt.mouse = ""
-vim.cmd('nnoremap <Left> <Nop>')
-vim.cmd('nnoremap <Down> <Nop>')
-vim.cmd('nnoremap <Up> <Nop>')
-vim.cmd('nnoremap <Right> <Nop>')
-vim.cmd('inoremap <Left> <Nop>')
-vim.cmd('inoremap <Down> <Nop>')
-vim.cmd('inoremap <Up> <Nop>')
-vim.cmd('inoremap <Right> <Nop>')
 
--- tabs & indent
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
-opt.smartindent = true
-opt.breakindent = true
-opt.expandtab = true
-opt.autoindent = true
-
-
--- search & clip
-opt.incsearch = false
-opt.smartcase = true
-opt.iskeyword:append("-")
-opt.clipboard:append("unnamedplus")
-
-
--- appearance
-opt.number = true
-opt.showmode = true
-opt.relativenumber = true
-opt.termguicolors  = true
-opt.cmdheight = 1
-opt.completeopt = "menuone,noinsert,noselect"
-opt.list = true
-opt.colorcolumn = '80'
-vim.cmd('hi Normal guibg=#00000000')
-opt.laststatus = 0
-
-
--- blank indicators
-opt.listchars:append "space:⋅"
-vim.wo.fillchars ='eob: '
-opt.guicursor = ""
-
-
--- behaviour
-opt.scrolloff = 10
-opt.hidden = true
-opt.errorbells = false
-opt.swapfile = false
-opt.backup = false
-opt.undofile = true
-opt.undodir = vim.fn.expand("~/.nvim/undodir")
-opt.backspace = "indent,eol,start"
+require("lazy").setup(
+  {
+    {import = 'plugins'}
+  },
+  options
+  )
---
+++ .config/nvim/lua/plugins/placeholder.lua
@@ -0,0 +1,3 @@
+local config = function ()
+end
+return {}
