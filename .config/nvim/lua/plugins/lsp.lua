e3fd0c73680692f82ed18a5fff18979c764fbae7
yref.boop <yref.boop@gmail.com>
2024-09-26 09:09:00 +0200

    feat: added treesitter

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,9 @@
 local config = function ()
   require'lspconfig'.pyright.setup{}
-  require'lspconfig'.pyright.setup{}
   require'lspconfig'.clangd.setup{}
   require'lspconfig'.matlab_ls.setup{}
   require'lspconfig'.lua_ls.setup{}
+  require'lspconfig'.asm_lsp.setup{}
 end
 
 return {
---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+local config = function ()
+  require'nvim-treesitter'.setup{
+    ensure_installed = {
+      "c",
+      "lua",
+      "python",
+      "matlab",
+      "latex",
+      "asm",
+    },
+    sync_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+  }
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  config = config
+}
