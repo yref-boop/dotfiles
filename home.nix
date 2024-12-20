e31f14e113a61a727c7923c6346f42641daaa1cc
yref-boop <yref.boop@gmail.com>
2024-12-20 21:35:18 +0100

    feat: added nvim options

--- home.nix
+++ home.nix
@@ -73,4 +73,68 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    extraLuaConfig = ''
+      local opt = vim.opt
+
+      -- controls
+      vim.g.mapleader = " "
+      opt.mouse = ""
+
+      -- tabs & indent
+      opt.tabstop = 2
+      opt.shiftwidth = 2
+      opt.softtabstop = 2
+      opt.smartindent = true
+      opt.breakindent = true
+      opt.expandtab = true
+      opt.autoindent = true
+
+      -- search & clip
+      opt.incsearch = false
+      opt.smartcase = true
+      opt.iskeyword:append("-")
+      opt.clipboard:append("unnamedplus")
+
+      -- appearance
+      opt.number = true
+      opt.showmode = true
+      opt.relativenumber = true
+      opt.termguicolors  = true
+      opt.cmdheight = 1
+      opt.completeopt = "menuone,noinsert,noselect"
+      opt.list = true
+      opt.colorcolumn = '80'
+      vim.cmd('hi Normal guibg=#00000000')
+      opt.laststatus = 0
+
+      -- blank indicators
+      opt.listchars:append "space:⋅"
+      vim.wo.fillchars ='eob: '
+      opt.guicursor = ""
+
+      -- behaviour
+      opt.scrolloff = 10
+      opt.hidden = true
+      opt.errorbells = false
+      opt.swapfile = false
+      opt.backup = false
+      opt.undofile = true
+      opt.undodir = vim.fn.expand("~/.nvim/undodir")
+      opt.backspace = "indent,eol,start"
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+    ];
+
+  };
+
 }
