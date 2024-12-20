d70448c3c2127e53d9986fbfe90a9b474177d0c7
yref-boop <yref.boop@gmail.com>
2024-12-20 23:19:21 +0100

    feat: added hyprland to home-manager

--- home.nix
+++ home.nix
@@ -74,6 +74,36 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  programs.kitty.enable = true;
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+
+      # bindings
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, F, exec, firefox"
+        "$mod, C, killactive"
+        "$mod, Q, exec,kitty"
+        "$mod, M, exit"
+        ", Print, exec, grimblast copy area"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (builtins.genList (i:
+            let ws = i + 1;
+            in [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+        )9)
+      );
+    };
+  };
+
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
