d3bece80668d3af67a61779447c09ac95a3b4ac4
yref-boop <yref.boop@gmail.com>
2025-04-01 00:54:24 +0200

    feat: added access to unstable

--- flake.nix
+++ flake.nix
@@ -4,22 +4,28 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.11";
-
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, nixpkgs-unstable, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+       overlay-unstable = final: prev: {
+        unstable = nixpkgs-unstable.legacyPackages.${prev.system};
+      };
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
         inherit system;
-        modules = [ ./configuration.nix ];
+        modules = [
+          # overlays-module for "pkgs.unstable" availability
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+          ./configuration.nix
+        ];
       };
     };
     homeConfigurations = {
