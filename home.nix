0b6789fa24e7248972ba0aad5f2cd6ea8afebcff
yref-boop <yref.boop@gmail.com>
2025-04-02 20:46:39 +0200

    feat: added initial niri config #2

--- home.nix
+++ home.nix
@@ -22,22 +22,225 @@
   # home-manager auto-management
   programs.home-manager.enable = true;
 
-  #
-  # window manager
-  #
+  #                #
+  # window manager #
+  #                #
+  # @https://github.com/YaLTeR/niri/wiki
   programs.niri = {
     enable = true;
-    # home-manager settings @https://github.com/sodiboo/niri-flake/blob/main/docs.md#homemodulesconfig
+
+    # home-manager settings:
+    # @https://github.com/sodiboo/niri-flake/blob/main/docs.md#homemodulesconfig
     settings = {
-      layout = { gaps = 14;};
-      binds = {"Mod+K".action.spawn = "kitty";};
-      binds = {"Mod+F".action.spawn = "firefox";};
+
+      input = {
+        keyboard = {
+          # repeat-delay = 600;
+          # repeat-rate = 25;
+          # track-layout = "window";
+          xkb = {
+            # layout = "us, pt";
+            # options = "grp:win_space_toggle";
+          };
+        };
+        touchpad = {
+          # off = true
+          # tap = false
+          # dwt = true
+          # dwtp = true
+          # natural-scroll
+          # accel-speed 0.2
+          # accel-profile "flat"
+          # scroll-method "two-finger"
+          # disabled-on-external-mouse
+        };
+        mouse = {
+          # off = true
+          # natural-scroll = true
+          # accel-speed = 0.2
+          # accel-profile = "flat"
+          # scroll-method = "no-scroll"
+        };
+        trackpoint = {
+          # off
+          # natural-scroll
+          # accel-speed 0.2
+          # accel-profile "flat"
+          # scroll-method "on-button-down"
+          # scroll-button 273
+          # middle-emulation
+        };
+        focus-follows-mouse.enable = true;
+        warp-mouse-to-focus = true;
+        # workspace-auto-back-and-forth = true;
+      };
+
+      outputs = {
+        "HDMI-A-1" = {
+          # off = true;
+          mode = {
+            width = 3440;
+            height = 1440;
+            refresh = 99.992;
+          };
+          # scale = 1;
+          # transform = flipped;
+          # position = {
+          #   x = 0;
+          #   y = 0;
+          #};
+        };
+        "eDP-1" = {
+          # off = true;
+          mode = {
+            width = 1920;
+            height = 1080;
+            refresh = 120.003;
+          };
+          # scale = 1;
+          # transform = flipped;
+          # position = {
+          #   x = 0;
+          #   y = 0;
+          #};
+        };
+      };
+
+      layout = {
+        gaps = 20;
+        # center-focused-column = "on-overflow";
+        preset-column-widths = [
+          {proportion = 1.0 / 3.0;}
+          {proportion = 1.0 / 2.0;}
+          {proportion = 2.0 / 3.0;}
+          {proportion = 1.0;}
+        ];
+
+        # preset-window-heights = [{proportion = ...;}]
+
+        default-column-width = {};
+
+        # focused window
+        focus-ring = {
+          enable = false;
+          width = 4;
+          active.color = "#000000";
+          inactive.color = "#000000";
+          # active.gradient = {
+          #  from = "#000000";
+          #  to = "#000000";
+          #  angle = 0;
+          #};
+          # inactive.gradient = {
+          #  from = "#000000";
+          #  to = "#000000";
+          #  angle = 0;
+          #};
+        };
+
+        border = {
+          enable = false;
+          width = 4;
+          active.color = "#000000";
+          inactive.color = "#000000";
+          # active.gradient = {
+          #  from = "#000000";
+          #  to = "#000000";
+          #  angle = 0;
+          #};
+          # inactive.gradient = {
+          #  from = "#000000";
+          #  to = "#000000";
+          #  angle = 0;
+          #};
+        };
+
+        shadow = {
+          enable = false;
+          # draw-behind-window = true;
+          softness = 30;
+          spread = 5;
+          offset = {
+            x = 0;
+            y = 0;
+          };
+          color = "#000000";
+        };
+
+        # outer gaps
+        struts = {
+          left = 60;
+          right = 60;
+          top = 60;
+          bottom = 60;
+        };
+
+      };
+
+      spawn-at-startup = [ ];
+
+      screenshot-path = "~/mídia/capturas/%Y-%m-%d-%H-%M-%S.png";
+
+      animations = {
+        # slowdown = 3.0;
+        shaders.window-resize = ''
+          vec4 resize_color(vec3 coords_curr_geo, vec3 size_curr_geo) {
+            vec3 coords_next_geo = niri_curr_geo_to_next_geo * coords_curr_geo;
+
+            vec3 coords_stretch = niri_geo_to_tex_next * coords_curr_geo;
+            vec3 coords_crop = niri_geo_to_tex_next * coords_next_geo;
+
+            // We can crop if the current window size is smaller than the next window
+            // size. One way to tell is by comparing to 1.0 the X and Y scaling
+            // coefficients in the current-to-next transformation matrix.
+            bool can_crop_by_x = niri_curr_geo_to_next_geo[0][0] <= 1.0;
+            bool can_crop_by_y = niri_curr_geo_to_next_geo[1][1] <= 1.0;
+
+            vec3 coords = coords_stretch;
+            if (can_crop_by_x)
+                coords.x = coords_crop.x;
+            if (can_crop_by_y)
+                coords.y = coords_crop.y;
+
+            vec4 color = texture2D(niri_tex_next, coords.st);
+
+            // However, when we crop, we also want to crop out anything outside the
+            // current geometry. This is because the area of the shader is unspecified
+            // and usually bigger than the current geometry, so if we don't fill pixels
+            // outside with transparency, the texture will leak out.
+            //
+            // When stretching, this is not an issue because the area outside will
+            // correspond to client-side decoration shadows, which are already supposed
+            // to be outside.
+            if (can_crop_by_x && (coords_curr_geo.x < 0.0 || 1.0 < coords_curr_geo.x))
+                color = vec4(0.0);
+            if (can_crop_by_y && (coords_curr_geo.y < 0.0 || 1.0 < coords_curr_geo.y))
+                color = vec4(0.0);
+
+            return color;
+          }
+        '';
+      };
+
+      window-rules = [
+        {
+          # rounded corners
+          geometry-corner-radius = {
+            bottom-left = 18.0;
+            bottom-right = 18.0;
+            top-left = 18.0;
+            top-right = 18.0;
+          };
+          clip-to-geometry = true;
+        }
+      ];
+
     };
   };
 
-  #
-  # PROGRAMS
-  #
+  #          #
+  # programs #
+  #          #
   programs = {
 
     bash = {
