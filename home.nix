4e22f6f04793ab475bee949b75d2cf3e3345dfd1
yref-boop <yref.boop@gmail.com>
2025-04-05 02:37:56 +0200

    chore: added default lsp binds explicitly

--- home.nix
+++ home.nix
@@ -433,18 +433,6 @@
     starship= {
       enable = true;
       enableBashIntegration = true;
-      # special characters  
-      # [](bg:#000000 fg:#000000)\\
-
-      # color_white = '#ffffff'
-      # color_black = '#000000'
-      # color_yellow = '#F18A31'
-      # color_orange = '#E45743'
-      # color_salmon = '#D43382'
-      # color_pink = '#B315CD'
-      # color_purple = '#BB65F0'
-      # color_indigo = '#AA85F1'
-      # color_blue = '#4166ED'
 
       settings = {
         add_newline = false;
@@ -584,12 +572,13 @@
         opt.undodir = vim.fn.expand("~/.nvim/undodir")
         opt.backspace = "indent,eol,start"
 
-        -- telescope
+        -- telescope keybinds
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
       '';
 
       plugins = let
@@ -701,6 +690,42 @@
               require'lspconfig'.gleam.setup{}
               require'lspconfig'.hls.setup{}
               require'lspconfig'.ocamllsp.setup{}
+
+              -- lsp keybinds
+              vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(ev)
+                  -- Enable completion triggered by <c-x><c-o>
+                  vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                  -- Buffer local mappings.
+                  -- See `:help vim.lsp.*` for documentation on any of the below functions
+                  local opts = { buffer = ev.buf }
+                  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                  vim.keymap.set('n', '<space>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                  end, opts)
+                  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+                  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                  vim.keymap.set('n', '<space>f', function()
+                    vim.lsp.buf.format { async = true }
+                  end, opts)
+                end,
+              })
+
+              vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+              vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+              vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+              vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
             '';
           }
           {
