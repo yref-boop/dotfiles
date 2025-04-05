d6da8c15c3ec8a525886c504c6d82cb7b0d3251d
yref-boop <yref.boop@gmail.com>
2025-04-05 02:59:42 +0200

    chore: added default telescope binds explicitly

--- home.nix
+++ home.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }:
 
+
 {
   # necessary info
   home.username = "yref-boop";
@@ -801,7 +802,13 @@
           {
             plugin = telescope-nvim;
             type = "lua";
-            config = ''require('telescope').setup()'';
+            config = ''require('telescope').setup({
+              vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+              vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+              vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+              vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+                  })
+            '';
           }
           {
             plugin = vimtex;
@@ -809,6 +816,7 @@
             config = '' vim.g.vimtex_view_method = "zathura" '';
           }
         ];
+
       };
 
     };
